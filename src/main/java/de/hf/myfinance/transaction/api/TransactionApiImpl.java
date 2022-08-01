9e07b705810894f7eca1a8faf73dc8dfda93e339
holger <holger@hfischer.org>
2022-08-01 17:10:04 +0200

    update frameworkversion

--- src/main/java/de/hf/myfinance/transaction/api/TransactionApiImpl.java
+++ src/main/java/de/hf/myfinance/transaction/api/TransactionApiImpl.java
@@ -1,6 +1,9 @@
 package de.hf.myfinance.transaction.api;
 
 import de.hf.myfinance.restapi.TransactionApi;
+import de.hf.myfinance.restmodel.RecurrentTransaction;
+import de.hf.myfinance.restmodel.Trade;
+import de.hf.myfinance.restmodel.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +21,46 @@ public class TransactionApiImpl implements TransactionApi {
     @Override
     public String index() {
         return "Hello my TransactionService";
+    }
+
+    @Override
+    public void addTrade(Trade trade) {
+
+    }
+
+    @Override
+    public void updateTrade(Trade trade) {
+
+    }
+
+    @Override
+    public void delRecurrentTransfer(String recurrentTransactionId) {
+
+    }
+
+    @Override
+    public void delTransfer(String transactionId) {
+
+    }
+
+    @Override
+    public void updateRecurrentTransaction(RecurrentTransaction recurrentTransaction) {
+
+    }
+
+    @Override
+    public void addRecurrentTransaction(RecurrentTransaction recurrentTransaction) {
+
+    }
+
+    @Override
+    public void addTransaction(Transaction transaction) {
+
+    }
+
+    @Override
+    public void updateTransaction(Transaction transaction) {
+
     }
 
 }
